9c54377aeafdf242b9c06f26dd2652e9538fd295
zishan100 <zishanansari111@gmail.com>
2024-05-06 20:51:07 +0530

    Counter App completed

--- src/App.js
+++ src/App.js
@@ -3,62 +3,24 @@ import './index.css';
 
 function App () {
   
-  let [formData , setFormData] = useState({
-    firstName:'',
-    lastName:'',
-    fullName:''
-  }) 
-
-  const handleInput = (e)=>{
-    const { name,value } = e.target;
-
-    setFormData({
-      ...formData,
-      [name]:value
-    })
-    // console.log(formData)
-  }
-
-  const formSubmit = (e)=>{
-    e.preventDefault();
-
-    const {firstName , lastName } = formData;
-    setFormData({
-      ...formData,
-      fullName:firstName+" "+lastName 
-    }) 
-  }
+  let [count,setCount ] = useState(0);
   
-
   return (
     <div >
-      <h2>Full Name Display</h2>
-      <form onSubmit={formSubmit} >
-        <label htmlFor="firstName" >First Name:</label>
-        <input 
-          type='text' 
-          id='firstName' 
-          name='firstName' 
-          value={formData.firstName}
-          onChange={handleInput} 
-          required
-        /><br/>
-        <label htmlFor="lastName" >Last Name:</label>
-        <input 
-          type='text' 
-          id='lastName'  
-          name='lastName' 
-          value={formData.lastName}
-          onChange={handleInput}
-          required 
-        /><br/>
-        <button type='submit' >Submit</button> 
-      </form>
-      {
-        formData.fullName && formData.fullName.trim() ? 
-        (<p>Full Name: {formData.fullName}</p>) : ''
-      }
-      
+      <h2>Counter App</h2>
+      <p>Count: {count}</p>
+      <button 
+        type='button' 
+        onClick={(e)=>{
+          setCount((prev)=>prev+1);
+        }} 
+      >Increment</button>
+      <button 
+        type='button'
+        onClick={(e)=>{
+          setCount((prev)=>prev-1)
+        }} 
+      >Decrement</button>
     </div>
   );
   
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 /*  
    # Stop watch miniAssignment ==> Completed
-
+   # Display FullName miniAssignment ==> Completed
 */
 
 // let [ running , setRunning ] = useState(false); 
@@ -40,3 +40,59 @@
 //     setelapsedTime(0);
 //     setRunning(false);
 //   }
+
+// let [formData , setFormData] = useState({
+//    firstName:'',
+//    lastName:'',
+//    fullName:''
+//  }) 
+
+//  const handleInput = (e)=>{
+//    const { name,value } = e.target;
+
+//    setFormData({
+//      ...formData,
+//      [name]:value
+//    })
+//    // console.log(formData)
+//  }
+
+//  const formSubmit = (e)=>{
+//    e.preventDefault();
+
+//    const {firstName , lastName } = formData;
+//    setFormData({
+//      ...formData,
+//      fullName:firstName+" "+lastName 
+//    }) 
+//  }
+
+// <div >
+// <h2>Full Name Display</h2>
+// <form onSubmit={formSubmit} >
+//   <label htmlFor="firstName" >First Name:</label>
+//   <input 
+//     type='text' 
+//     id='firstName' 
+//     name='firstName' 
+//     value={formData.firstName}
+//     onChange={handleInput} 
+//     required
+//   /><br/>
+//   <label htmlFor="lastName" >Last Name:</label>
+//   <input 
+//     type='text' 
+//     id='lastName'  
+//     name='lastName' 
+//     value={formData.lastName}
+//     onChange={handleInput}
+//     required 
+//   /><br/>
+//   <button type='submit' >Submit</button> 
+// </form>
+// {
+//   formData.fullName && formData.fullName.trim() ? 
+//   (<p>Full Name: {formData.fullName}</p>) : ''
+// }
+
+// </div>
